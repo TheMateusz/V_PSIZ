2e979b3fbb335d02afcfd85b93932492633c0545
TheMateusz <matimatusiak109@gmail.com>
2022-06-25 14:08:06 +0200

    Dodanie dodatkowych struktur

--- main.cpp
+++ main.cpp
@@ -12,6 +12,32 @@
     char B;
     }
 
+    //Struktura naglowek
+    struct naglowek
+    {
+    int Size;
+    int Width;
+    int Height;
+    short Planes;
+    short BitCount;
+    int Compression;
+    int izeImage;
+    int XPelsPerMeter;
+    int YPelsPerMeter;
+    int CUsed;
+    int CImportant;
+    } zdjecie;
+
+    //Struktura naglowek plik
+    struct FileHeader
+    {
+    short fType;
+    int fSize;
+    short fReserved1;
+    short fReserved2;
+    int fOffBits;
+    } plik;
+
 int main(int arc, char * argv[]) {
 
     //Otwieranie pliku
